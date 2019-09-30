57b65e8aef310992b9bbfca0e4094afca0888c9b
Andy Fiedler <andyfidd@gmail.com>
2019-09-30 15:54:47 -0500

    banjo replaced supergoose file to solve async issue

---
+++ src/routes/books.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const express = require('express');
+const router = express.Router();
+const User = require('./users-model');
+
+
+router.get('/books', handleGetAll);
+router.get('/books/:id', handleGetOne);
+
+// Route Handlers
+function handleGetAll(req, res, next) {
+  let books = {
+    count: 3,
+    results: [
+      { title:'Moby Dick' },
+      { title:'Little Women' },
+      { title: 'Eloquent Javascript' },
+    ],
+  };
+  res.status(200).json(books);
+}
+
+function handleGetOne(req, res, next) {
+  let book = {
+    title:'Moby Dick',
+  };
+  res.status(200).json(book);
+}
+
+module.exports = router;
